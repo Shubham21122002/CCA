8272d42b9c0e3fdae1f1f01a6a83747b9ceebef3
shubham thitame <thitameshubham10@gmail.com>
2024-10-03 20:07:04 +0530

    na

--- tsp.cpp
+++ tsp.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+#define INT_MAX 999
 int n = 4;
 int dist[10][10] = {
     {0, 1, 2, 3},
@@ -15,7 +16,7 @@ int tsp(int mask, int pos) {
     }
 
     // Try to goto unvisited city
-    int ans = INT32_MAX;
+    int ans = INT_MAX;
     for(int city = 0 ; city < n ; city++) {
         if(mask&(1<<city) == 0) {
             int newAns = dist[pos][city] + tsp(mask | (1<<city), city);
@@ -25,7 +26,8 @@ int tsp(int mask, int pos) {
     return ans;
 }
 
-int main()
+int main() 
 {
+    cout<<"MST : "<<tsp(1, 0);
  return 0;
 }
