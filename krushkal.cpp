787749707d54a6313838e384e7513487b6c6cd76
shubham thitame <thitameshubham10@gmail.com>
2024-11-07 16:41:56 +0530

    na

--- krushkal.cpp
+++ krushkal.cpp
@@ -1,40 +1,42 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <iostream>
+#include <climits> // For INT_MAX
+
+using namespace std;
 
 int i, j, k, a, b, u, v, n, ne = 1;
-int min, mincost = 0, cost[9][9], parent[9];
+int minimum, mincost = 0, cost[9][9], parent[9] = {0};
 
 int find(int);
 int uni(int, int);
 
 int main()
 {
-    printf("\n\tImplementation of Kruskal's algorithm\n");
-    printf("\nEnter the no. of vertices:");
-    scanf("%d",&n);
-    printf("\nEnter the cost adjacency matrix:\n");
+    cout << "\n\tImplementation of Kruskal's algorithm\n";
+    cout << "\nEnter the number of vertices: ";
+    cin >> n;
+    cout << "\nEnter the cost adjacency matrix:\n";
 
     for (i = 1; i <= n; i++)
     {
-        for(j = 1; j <= n; j++)
+        for (j = 1; j <= n; j++)
         {
-            scanf("%d",&cost[i][j]);
-            if(cost[i][j] == 0)
-                cost[i][j] = 999;
+            cin >> cost[i][j];
+            if (cost[i][j] == 0)
+                cost[i][j] = INT_MAX; // Using INT_MAX instead of 999 for infinity
         }
     }
 
-    printf("The edges of Minimum Cost Spanning Tree are\n");
+    cout << "The edges of Minimum Cost Spanning Tree are\n";
 
-    while(ne < n)
+    while (ne < n)
     {
-        for(i = 1, min = 999; i <= n; i++)
+        for (i = 1, minimum = INT_MAX; i <= n; i++)
         {
-            for(j = 1; j <= n; j++)
+            for (j = 1; j <= n; j++)
             {
-                if(cost[i][j] < min)
+                if (cost[i][j] < minimum)
                 {
-                    min = cost[i][j];
+                    minimum = cost[i][j];
                     a = u = i;
                     b = v = j;
                 }
@@ -42,28 +44,29 @@ int main()
         }
         u = find(u);
         v = find(v);
-        if(uni(u,v))
+        if (uni(u, v))
         {
-            printf("%d edge (%d, %d) =%d\n", ne++, a, b, min);
-            mincost += min;
+            cout << ne++ << " edge (" << a << ", " << b << ") = " << minimum << endl;
+            mincost += minimum;
         }
-        cost[a][b] = 999;
+        cost[a][b] = INT_MAX; // Mark as visited
     }
-    printf("\n\tMinimum cost = %d\n",mincost);
+    cout << "\n\tMinimum cost = " << mincost << endl;
+    return 0;
 }
 
 int find(int i)
 {
-    while(parent[i])
-    i = parent[i];
+    while (parent[i])
+        i = parent[i];
     return i;
 }
 
-int uni(int i,int j)
+int uni(int i, int j)
 {
-    if(i!=j)
+    if (i != j)
     {
-        parent[j]=i;
+        parent[j] = i;
         return 1;
     }
     return 0;
