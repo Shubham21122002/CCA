6301607c5cc1d41cdbfd06cdba3a262b4e1d431a
shubham thitame <thitameshubham10@gmail.com>
2024-11-08 09:20:54 +0530

    kruskal_update

--- krushkal.cpp
+++ krushkal.cpp
@@ -1,57 +1,54 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-struct Edge {
-    int u, v, weight;
-    bool operator<(const Edge& other) const {
-        return weight < other.weight;
-    }
-};
+const int MAX = 1e4; // Maximum number of nodes
+int parent[MAX];
 
-int findParent(int u, vector<int>& parent) {
-    if (u != parent[u])
-        parent[u] = findParent(parent[u], parent);
-    return parent[u];
+// Find function with path compression
+int find(int u) {
+    if (parent[u] == u)
+        return u;
+    return parent[u] = find(parent[u]);
 }
 
-void unionSets(int u, int v, vector<int>& parent, vector<int>& rank) {
-    u = findParent(u, parent);
-    v = findParent(v, parent);
-    if (u != v) {
-        if (rank[u] < rank[v])
-            swap(u, v);
-        parent[v] = u;
-        if (rank[u] == rank[v])
-            rank[u]++;
-    }
+// Union function for union by rank
+void unionSets(int u, int v) {
+    int rootU = find(u);
+    int rootV = find(v);
+    if (rootU != rootV)
+        parent[rootU] = rootV;
 }
 
-int kruskal(int n, vector<Edge>& edges) {
-    sort(edges.begin(), edges.end());
-    vector<int> parent(n), rank(n, 0);
-    for (int i = 0; i < n; ++i) parent[i] = i;
+int kruskal(int n, vector<pair<int, pair<int, int>>>& edges) {
+    int mst_cost = 0;
+    sort(edges.begin(), edges.end()); // Sort edges by weight
 
-    int mstWeight = 0;
-    for (const auto& edge : edges) {
-        if (findParent(edge.u, parent) != findParent(edge.v, parent)) {
-            mstWeight += edge.weight;
-            unionSets(edge.u, edge.v, parent, rank);
-            cout << "Edge: " << edge.u << " - " << edge.v << " | Weight: " << edge.weight << endl;
+    // Initialize each node to be its own parent
+    for (int i = 0; i < n; ++i)
+        parent[i] = i;
+
+    for (auto& edge : edges) {
+        int weight = edge.first;
+        int u = edge.second.first;
+        int v = edge.second.second;
+
+        // Check if u and v are in the same set
+        if (find(u) != find(v)) {
+            mst_cost += weight;
+            unionSets(u, v);
         }
     }
-    return mstWeight;
+    return mst_cost;
 }
 
 int main() {
-    int n = 4; // Number of vertices
-    vector<Edge> edges = {
-        {0, 1, 10}, {0, 2, 6}, {0, 3, 5}, {1, 3, 15}, {2, 3, 4}
+    int n = 4; // Number of nodes
+    vector<pair<int, pair<int, int>>> edges = {
+        {10, {0, 1}}, {6, {0, 2}}, {5, {0, 3}}, {15, {1, 3}}, {4, {2, 3}}
     };
 
-    int mstWeight = kruskal(n, edges);
-    cout << "Total weight of MST: " << mstWeight << endl;
+    cout << "Minimum Cost of MST: " << kruskal(n, edges) << endl;
     return 0;
 }
