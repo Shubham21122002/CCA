972db41a724307ad55a6a6ee7f018ecf56fa3275
shubham thitame <thitameshubham10@gmail.com>
2025-01-09 14:48:09 +0530

    na

--- NQueen.cpp
+++ NQueen.cpp
@@ -1,61 +1,41 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
+#include<iostream>
+#include<vector>
 using namespace std;
+bool isSafe(int row, int col, vector<string> &board, int n) {
 
-void backtrack(int row, int n, vector<int>& queens, vector<vector<string>>& solutions);
-bool isValid(int row, int col, const vector<int>& queens);
-
-vector<vector<string>> solveNQueens(int n) {
-    vector<vector<string>> solutions;
-    vector<int> queens(n, -1); // To store column positions of queens
-    backtrack(0, n, queens, solutions);
-    return solutions;
 }
-
-void backtrack(int row, int n, vector<int>& queens, vector<vector<string>>& solutions) {
-    if (row == n) {
-        // If all queens are placed, create a solution board
-        vector<string> board(n, string(n, '.'));
-        for (int i = 0; i < n; ++i) {
-            board[i][queens[i]] = 'Q';
-        }
-        solutions.push_back(board);
+int solve(int col, vector<string> &board,vector<vector<string>> ans, int n) {
+    //base cond
+    if(col==n) {
+        ans.push_back(board);
         return;
     }
-
-    for (int col = 0; col < n; ++col) {
-        if (isValid(row, col, queens)) {
-            queens[row] = col; // Place the queen
-            backtrack(row + 1, n, queens, solutions); // Recur to place the next queen
-            queens[row] = -1; // Backtrack
+    
+    for(int row = 0 ; row < n ; row++) {
+        if(isSafe(row,col,board,n)) {
+            board[row][col] = 'Q';
+            solve(col+1, board,ans,n);
+            board[row][col] = '.';
         }
     }
 }
-
-bool isValid(int row, int col, const vector<int>& queens) {
-    for (int i = 0; i < row; ++i) {
-        // Check if the column or diagonals are threatened
-        if (queens[i] == col || 
-            queens[i] - i == col - row || 
-            queens[i] + i == col + row) {
-            return false;
-        }
+vector<vector<string>> solveNQueen(int n) {
+    vector<vector<string>> ans;
+    vector<string> board(n);
+    string s(n,'.');
+    for(int i = 0 ; i < n ; i++ ) {
+        board[i] = s;
     }
-    return true;
+   solve(0, board, ans, n);
+   return ans;
 }
+int main()
+{
+    int n;
+    cout<<"Enter the size of board : ";
+    cin>>n;
+    vector<vector<string>> ans = solveNQueen(n);
 
-int main() {
-    int n = 4; // Change this value for different sizes of the board
-    vector<vector<string>> solutions = solveNQueens(n);
 
-    for (const auto& solution : solutions) {
-        for (const auto& row : solution) {
-            cout << row << endl;
-        }
-        cout << endl;
-    }
-
-    return 0;
+ return 0;
 }
